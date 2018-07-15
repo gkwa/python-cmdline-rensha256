13832ab6183b8dd0aea50f113ecbd6d03524961c
Matthew Taylor Monacelli <monacem@amazon.com>
2018-07-14 22:23:51 -0700

    Initial

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+
+import sys
+if sys.version_info < (3, 0):
+    sys.exit('Sorry, Python < 3.0 is not supported')
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('rensha256/rensha256.py').read(),
+    re.M
+).group(1)
+
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name="cmdline-rensha256",
+    python_requires='>3',
+    packages=["rensha256"],
+    entry_points={
+        "console_scripts": ['rensha256 = rensha256.rensha256:main']
+    },
+    install_requires=[
+        "pathlib"
+    ],
+    version=version,
+    description="Python command line application bare bones template.",
+    long_description=long_descr,
+    author="Taylor Monacelli",
+    author_email="taylormonacelli@gmail.com"
+)
